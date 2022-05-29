6a61244de4d50aae1b85dfab8ff7fb5af36a80dc
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-05-29 14:13:46 -0300

    Correções no primeiro programa Java

--- Aula4 - Primeiro Programa em Java/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
+++ Aula4 - Primeiro Programa em Java/PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -1,7 +1,8 @@
-package com.mycompany.primeiroprograma;
+package primeiroprograma;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        System.out.print("Olá, Mundo!");
+        System.out.println("Olá, Mundo!");
     }
+    
 }
