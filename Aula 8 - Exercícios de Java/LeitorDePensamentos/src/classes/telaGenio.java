d1439080f41e4216d8674c86f3e5de0ea0132faf
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-06 02:31:08 -0300

    Criação do repositório da Aula 8 - Exercícios de Java

---
+++ Aula 8 - Exercícios de Java/LeitorDePensamentos/src/classes/telaGenio.java
@@ -0,0 +1,90 @@
+package classes;
+
+import java.awt.Font;
+
+public class telaGenio extends javax.swing.JFrame {
+
+    public telaGenio() {
+        initComponents();
+        lblFrase.setText("<html>Vou pensar em um valor entre 1 e 5. Tente adivinhar</html>");
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel2 = new javax.swing.JLabel();
+        lblFrase = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        txtVal = new javax.swing.JSpinner();
+        btnPalpite = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setFocusable(false);
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/genio-pequeno.png"))); // NOI18N
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 60, -1, -1));
+
+        lblFrase.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblFrase.setText("Frase");
+        getContentPane().add(lblFrase, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 30, 110, 50));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/balao-pequeno.png"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 10, -1, -1));
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel4.setText("Valor");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 180, -1, -1));
+
+        txtVal.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        txtVal.setModel(new javax.swing.SpinnerNumberModel(1, 1, 5, 1));
+        getContentPane().add(txtVal, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 180, 70, -1));
+
+        btnPalpite.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        btnPalpite.setText("Palpite");
+        btnPalpite.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPalpiteActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnPalpite, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 230, 110, -1));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnPalpiteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPalpiteActionPerformed
+        double n = 1 + Math.random() * (6-1);
+        int valor = (int) n;
+        
+        int num = Integer.parseInt(txtVal.getValue().toString());
+        
+        String f1 = "ACERTOU!";
+        String f2 = "<html>ERROU! Eu pensei no valor <html>" + valor;
+        
+        String res = (valor == num)?f1:f2;
+        
+        lblFrase.setFont(new Font("Arial Black", Font.PLAIN, 13));
+        
+        lblFrase.setText(res);
+    }//GEN-LAST:event_btnPalpiteActionPerformed
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new telaGenio().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnPalpite;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel lblFrase;
+    private javax.swing.JSpinner txtVal;
+    // End of variables declaration//GEN-END:variables
+}
